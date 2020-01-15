ee352abda96d09b1ac27f48f76597a9f5a51c2ab
kicyiu <albertokicyiu@gmail.com>
2020-01-15 15:09:30 -0300

    Add task search bar

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Alberto Tsang on 2020/1/15.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    // MARK: - TableView Datasource Methods
+    
+    
+    // MARK: - TableView Delegate Methods
+    
+    
+    // MARK: - Add New Categories
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    
+    // MARK: - Data Manipulation Methods
+}
